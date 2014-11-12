8491d96f3ab3e843199f6b47ded4b7825f1e6919
Brian Lindblom <lindblombr@ornl.gov>
2014-11-12 01:06:06 -0800

    Keep trying...

--- manifests/qmaster.pp
+++ manifests/qmaster.pp
@@ -29,15 +29,22 @@ define gridengine::qmaster (
     "/var/spool/sge":         ensure => "directory", mode => 755;
     "/var/spool/sge/qmaster": ensure => "directory", mode => 755;
     "/var/spool/sge/db":      ensure => "directory", mode => 755;
+    "/var/spool/sge/qmaster/defaultcfg": source => "$mod_file_path/defaultcfg", mode => 600;
   }
 
   exec {
     "berkeley_db_init":
-      path    => ["/bin","/usr/bin","$sgeroot/utilbin/lx-amd64"],
-      cwd     => "$sgeroot/utilbin/lx-amd64",
-      command => "spoolinit berkeleydb $sgeroot/lib/lx-amd64/libspoolb /var/spool/sge/db init",
-      onlyif  => "test ! -f /var/spool/sge/db/sge",
-      require => File["/var/spool/sge/db"];
+      path        => ["/bin","/usr/bin","$sgeroot/utilbin/lx-amd64"],
+      environment => ["SGE_ROOT=$sgeroot", "SGE_CELL=$sgecell"],
+      cwd         => "$sgeroot/utilbin/lx-amd64",
+      command     => [
+        "spoolinit berkeleydb $sgeroot/lib/lx-amd64/libspoolb /var/spool/sge/db init",
+        "spooldefaults configuration /var/spool/sge/qmaster/defaultcfg",
+        "spooldefaults complexes $sgeroot/util/resources/centry",
+        "spooldefaults pes $sgeroot/util/resources/pes"
+        ],
+      onlyif      => "test ! -f /var/spool/sge/db/sge",
+      require     => [ File["/var/spool/sge/db"], File["/var/spool/sge/qmaster/defaultcfg"] ];
   }
 
   service { 
