646f688f8ac3044ef0ace10aae7a69ed454b4063
Brian Lindblom <lindblombr@ornl.gov>
2014-11-12 01:16:30 -0800

    Keep trying...

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,5 +42,7 @@ class gridengine ($sgeroot, $sgecell, $sgemaster, $sgecluster) {
     "$sgecommon/settings.sh":     source  => "$mod_file_path/settings.sh";
     "$sgecommon/settings.csh":    content => template("gridengine/settings.csh.erb");
     "/etc/sysconfig/gridengine":  content => template("gridengine/gridengine.erb");
+    "/etc/profile.d/sge.sh":      ensure => link, target => "$sgecommon/settings.sh";
+    "/etc/profile.d/sge.csh":     ensure => link, target => "$sgecommon/settings.csh";
   }
 }
