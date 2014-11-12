3eb45e514931a79bc457c87a797603c22f6c5ea1
Brian Lindblom <lindblombr@ornl.gov>
2014-11-12 00:44:45 -0800

    Keep trying...

--- manifests/qmaster.pp
+++ manifests/qmaster.pp
@@ -27,8 +27,17 @@ define gridengine::qmaster (
     "$sgecommon/system.jsv":  source  => "$mod_file_path/system.jsv", mode => 555;
     "/etc/init.d/sgemaster":  source  => "$mod_file_path/sgemaster", mode => 700;
     "/var/spool/sge":         ensure => "directory", mode => 755;
-    "/var/spool/sge/qmaster":         ensure => "directory", mode => 755;
-    "/var/spool/sge/db":         ensure => "directory", mode => 755;
+    "/var/spool/sge/qmaster": ensure => "directory", mode => 755;
+    "/var/spool/sge/db":      ensure => "directory", mode => 755;
+  }
+
+  exec {
+    "berkeley_db_init":
+      path    => ["/bin","/usr/bin","$sgeroot/utilbin/lx-amd64"],
+      cwd     => "$sgeroot/utilbin/lx-amd64",
+      command => "spoolinit berkeleydb $sgeroot/lib/lx-amd64/libspoolb /var/spool/sge/db init",
+      onlyif  => "test ! -f /var/spool/sge/db/sge",
+      require => File["/var/spool/sge/db"];
   }
 
   service { 
@@ -41,7 +50,9 @@ define gridengine::qmaster (
         Class['gridengine'], 
         File["$sgecommon/system.jsv"], 
         File["/etc/init.d/sgemaster"],
-        File["/etc/sgeinstall.conf"]
+        File["/etc/sgeinstall.conf"],
+        File["/var/spool/sge/qmaster"],
+        Exec["berkeley_db_init"]
       ],
       subscribe => Class['gridengine'];
   }
